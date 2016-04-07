a3fd1fbd80e0381159bac194863ff16d3df3fab8
usuario <rodmen@gmail.com>
2016-04-07 11:19:06 -0300

    Bulk update of data of videos in the provider

--- Library/Providers/ThePlatformProvider.php
+++ Library/Providers/ThePlatformProvider.php
@@ -257,4 +257,29 @@ class ThePlatformProvider implements ProviderInterface
         return $videos;
     }
 
+    /**
+     * Bulk update of a properties of a video
+     * @param $videoData is an associative array with the id of the video in the key "id", the rest of entries are couples property_to_update => value
+     * @return null
+     */
+    public function updateVideosProperties($videoData)
+    {
+        $token = $this->_auth['token'];
+        $url = 'http://data.media.' . $this->getBaseUrl() .'/media/data/Media/list?'
+            . 'schema=1.8.0&form=json&method=put&'
+            . 'token=' . $token . '&account=' . $this->_account;
+
+        foreach ($videoData as $k => $data) {
+            $videoData[$k]['id'] = 'http://data.media.' . $this->getBaseUrl() .'/media/data/Media/' . $videoData[$k]['id'];
+        }
+
+        $data = array(
+            '$xmlns' => array('fox' => 'http://xml.fox.com/fields'),
+            'entries' => $videoData
+        );
+        $encodedData = json_encode($data);
+
+        $response = $this->_request($url, $encodedData);
+        return true;
+    }
 }
